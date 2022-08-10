79ddd5b33f3d3f5097f36584699ae8022222438e
Malithi2021 <abayadeeramr.20@itfac.mrt.ac.lk>
2022-08-10 21:14:00 +0530

    Add project files.

---
+++ WebApplication2/Controllers/PersonController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    
+    public class PersonController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            var people = new List<Person>();
+            people.Add(new Person() { No=1,Name="Malithi", LastName="Perera" , Age=21, Dept="it" , Salary=2000});
+            people.Add(new Person() { No = 2, Name = "Malithi", LastName = "Perera", Age = 21, Dept = "it", Salary = 2000 });
+            people.Add(new Person() { No = 3, Name = "Malithi", LastName = "Perera", Age = 21, Dept = "it", Salary = 2000 });
+            people.Add(new Person() { No = 4, Name = "Malithi", LastName = "Perera", Age = 21, Dept = "it", Salary = 2000 });
+            people.Add(new Person() { No = 5, Name = "Malithi", LastName = "Perera", Age = 21, Dept = "it", Salary = 2000 });
+            people.Add(new Person() { No = 6, Name = "Malithi", LastName = "Perera", Age = 21, Dept = "it", Salary = 2000 });
+            return View(people);
+        }
+
+        public ActionResult CreateNew()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+    
+
+   
+}
---
+++ WebApplication2/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Person
+    {
+        public int No { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Dept { get; set; }
+
+        public decimal Salary { get; set; }
+
+    }
+}
